5e149be87079f67f92dc715ba7a5f5140d043ef0
zhussain-uxli <89850348+zhussain-uxli@users.noreply.github.com>
2021-09-02 18:13:20 +0530

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -2,19 +2,27 @@ $appId = "10ea3bbe-7c7f-4a06-8b9a-c558e5a996a1"
 $azPwd = "q_S2_H1E3_lZ-vNN1zqfVSgT9AjAJ_6Q84"
 $tenant = '5a934bcd-767d-410c-8570-d0977d20aaf4'
 $pswd = $azPwd | ConvertTo-SecureString -AsPlainText -Force 
+
+$checkmodule = Get-Module -ListAvailable | Where-Object { $_.Name -eq "Az" }
+    if (!$checkmodule) {    
+        Write-Host "Installing dbatools module" -ForegroundColor Cyan
+        Start-Process powershell.exe -ArgumentList "-Command Install-Module Az" -Verb -Wait
+    } 
+
 #$azCreds = Get-Credential
 $azCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $appId, $pswd
 Connect-AzAccount -Credential $azCreds -TenantId $tenant -ServicePrincipal
 $passwrd  = Get-AzKeyVaultSecret -VaultName "JbtDevopsKey" -Name "Password" -AsPlainText
 Write-Host "Secret is $passwrd" -ForegroundColor Cyan
 $secureStringPwd = ConvertTo-SecureString $passwrd -AsPlainText -Force 
-$loginName = "zhussain@uxli.co.uk" 
-#$synapseSqlName = "mssql-dev.uxli.com" 
-$SqlHostName = "mssql-dev.uxli.com" 
-$databaseName = "devops" 
+#$loginName = "synapse_admin"
+$loginName = Get-AzKeyVaultSecret -VaultName "JbtDevopsKey" -Name "Username" -AsPlainText
+#$SqlHostName = "synw-jbt-dw-dev1.sql.azuresynapse.net" 
+$SqlHostName = Get-AzKeyVaultSecret -VaultName "JbtDevopsKey" -Name "Hostname" -AsPlainText
+#$databaseName = "synsqljbtdw" 
+$databaseName =  Get-AzKeyVaultSecret -VaultName "JbtDevopsKey" -Name "DBName" -AsPlainText
 #$login = Get-Credential -Message "Enter your SQL on-demand password" -UserName $loginName 
 $login = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $loginName, $secureStringPwd
-Write-Host "creds - $login"
 $ScriptPath = Get-Location
 #$ScriptPath = "Release2.0"
 
